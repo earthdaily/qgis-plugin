461b6a340b5d5dd2623586694a1d0c010c5933cb
Muhammad Yarjuna Rohmat <myarjunar@gmail.com>
2019-04-12 21:48:47 +0700

    fix processing alg behavior

--- geosys/utilities/downloader.py
+++ geosys/utilities/downloader.py
@@ -89,7 +89,11 @@ def extract_zip(zip_path, destination_base_path):
     zip_file = zipfile.ZipFile(handle)
     for name in zip_file.namelist():
         extension = os.path.splitext(name)[1]
-        output_final_path = '%s%s' % (destination_base_path, extension)
+        _, requested_extension = os.path.splitext(destination_base_path)
+        if requested_extension:
+            output_final_path = destination_base_path
+        else:
+            output_final_path = '%s%s' % (destination_base_path, extension)
         output_file = open(output_final_path, 'wb')
         output_file.write(zip_file.read(name))
         output_file.close()
