fb46c8328d3c7ce887516a3e8cf1731e80d73018
vermeulendivan <vermeulendivan@gmail.com>
2022-02-15 18:30:33 +0200

    Test methods for clear_combo_box and populate_map_products

--- geosys/test/test_geosys_dockwidget.py
+++ geosys/test/test_geosys_dockwidget.py
@@ -12,6 +12,7 @@ import unittest
 
 from geosys.test.utilities import get_qgis_app
 from geosys.ui.widgets.geosys_dockwidget import GeosysPluginDockWidget
+from geosys.utilities.settings import setting
 from PyQt5.QtWidgets import QComboBox
 
 __copyright__ = "Copyright 2019, Kartoza"
@@ -40,17 +41,39 @@ class GeosysPluginDockWidgetTest(unittest.TestCase):
     def test_clear_combo_box(self):
         """Test if the clear_combo_box method works as it should."""
 
-        test_list = ['First', 'Second', 'Third']
+        test_list = ['First', 'Second', 'Third']  # A list of items which will be added to the combobox
         test_cb = QComboBox()
         test_cb.addItems(test_list)
 
-        GeosysPluginDockWidget.clear_combo_box(test_cb)
+        # Clears the combobox of all items
+        GeosysPluginDockWidget.clear_combo_box(self.dockwidget, test_cb)
 
-        expected_count = 0
+        expected_count = 0  # The combobox should now be empty
         cb_count = test_cb.count()
 
-        message = 'Expected %s items in the combobox, but got %s' % (expected_count, str(cb_count))
-        self.assertEqual(expected_count, str(cb_count), message)
+        message = 'Expected %s items in the combobox, but got %s' % (str(expected_count), str(cb_count))
+        self.assertEqual(str(expected_count), str(cb_count), message)
+
+    def test_populate_map_products(self):
+        """Test whether the combobox is populated correctly.
+        If the zone is set to US, the soil map type can be included.
+        If the zone is set to EU, the soil map type should be excluded.
+        """
+
+        GeosysPluginDockWidget.populate_map_products(self.dockwidget)
+
+        key_us = 'geosys_region_na'
+        us_option = setting(key_us, expected_type=bool, qsettings=self.dockwidget.settings)
+        if us_option:  # Expected number of items for US (soil map included)
+            expected_count = 21
+        else:  # Expected number of items for EU (soil map excluded)
+            expected_count = 20
+
+        combobox = self.dockwidget.map_product_combo_box
+        cb_count = combobox.count()
+
+        message = 'Expected %s items in the combobox, but got %s' % (str(expected_count), str(cb_count))
+        self.assertEqual(str(expected_count), str(cb_count), message)
 
 
 if __name__ == "__main__":
