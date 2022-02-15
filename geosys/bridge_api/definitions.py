63c083ad127d276190a3944472e28d5e32ce5049
vermeulendivan <vermeulendivan@gmail.com>
2022-02-15 17:09:51 +0200

    Fixed code conflict in definitions.py

--- geosys/bridge_api/definitions.py
+++ geosys/bridge_api/definitions.py
@@ -64,7 +64,6 @@ yield_variability_map = {
     'key': 'yield-variability-map',
     'endpoint': 'yield-variability-map'
 }
-
 reflectance_map = {
     'key': 'reflectance-map',
     'endpoint': 'reflectance-map'
