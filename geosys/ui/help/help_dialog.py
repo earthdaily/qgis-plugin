8be73f5d68850fdef3f25117cd18e73987638b17
Samweli <samweli@kartoza.com>
2022-02-21 23:16:20 +0300

    open links in the help and about dialog with the default browser

--- geosys/ui/help/help_dialog.py
+++ geosys/ui/help/help_dialog.py
@@ -4,7 +4,7 @@
 # This import is to enable SIP API V2
 # noinspection PyUnresolvedReferences
 import qgis  # NOQA pylint: disable=unused-import
-from qgis.PyQt import QtGui, QtWidgets
+from qgis.PyQt import QtGui, QtWidgets, QtWebKitWidgets
 from qgis.PyQt.QtCore import Qt
 
 from geosys.utilities.help import get_help_html
@@ -39,4 +39,14 @@ class HelpDialog(QtWidgets.QDialog, FORM_CLASS):
         icon = resources_path('img', 'icons', 'icon.png')
         self.setWindowIcon(QtGui.QIcon(icon))
 
+        # Make the html links open on the default browser instead
+        # of opening the current help dialog.
+        self.help_web_view.page().setLinkDelegationPolicy(
+            QtWebKitWidgets.QWebPage.DelegateAllLinks
+        )
+        self.help_web_view.linkClicked.connect(self.link_clicked)
+
         self.help_web_view.setHtml(get_help_html(message))
+
+    def link_clicked(self, url):
+        QtGui.QDesktopServices.openUrl(url)
