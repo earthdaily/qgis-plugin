be637cc41cb5553c201e733aa55b5f69d2123d1c
VincentLel <vincent.lelandais@geosys.com>
2022-08-15 09:51:58 -0500

    clean gh-pages before adding md content

--- geosys/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-"""Package for geosys."""
-# This import is to enable SIP API V2
-# noinspection PyUnresolvedReferences
-# import qgis  # pylint: disable=unused-import
-import logging
