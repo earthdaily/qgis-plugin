2b3137f418d54481fc5dcb2cbc91d5d80b713f71
Samweli <smwltwesa6@gmail.com>
2024-12-31 18:43:06 +0300

    version 1.2.2 dev

--- geosys/ui/widgets/geosys_coverage_downloader.py
+++ geosys/ui/widgets/geosys_coverage_downloader.py
@@ -948,6 +948,14 @@ def create_map(
                 'Id': image_id
             }
         }
+
+    elif map_type_key == SOIL['key']:
+        request_data = {
+            'SeasonField': {
+                'Id': season_field_id,
+                'geometry': season_field_geom,
+            }
+        }
     else:
         request_data = {
             'SeasonField': {
@@ -967,6 +975,9 @@ def create_map(
     bridge_api = BridgeAPI(
         *credentials_parameters_from_settings(),
         proxies=QGISSettings.get_qgis_proxy())
+
+    if map_type_key == SOIL['key']:
+        data = request_data
 
     if map_type_key == SAMPLE_MAP['key']:
 
@@ -1005,6 +1016,8 @@ def create_map(
             sample_map_id=None,
             zone_count=zone_count,
             **data)
+
+    log(f"used data for SOIL {data} \n map json {field_map_json}")
 
     result, message = download_field_map(
         field_map_json=field_map_json,
